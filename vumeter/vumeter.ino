075db560853b08bb89f88bf3303cc1e58875c4c9
carlaespino <cespino@elisava.net>
2016-10-17 22:41:39 +0200

    Update vumeter.ino

--- vumeter/vumeter.ino
+++ vumeter/vumeter.ino
@@ -3,7 +3,7 @@ int led12 = 12;
 int led11 = 11;
 int led10 = 10;
 int led9 = 9;
-//int lastvalue=0;
+
 void setup() {
   
   pinMode(led9, OUTPUT);
@@ -17,20 +17,20 @@ void setup() {
 void loop() {
 
 
-int valueldr = analogRead(ldr);
-int value1 = map(valueldr,0,1023,0,4);
-int value2 = map(valueldr,0,1023,1,5);
+int valueldr = analogRead(ldr);  //here we read the potentiometer value
+int value1 = map(valueldr,0,1023,0,4); //we assign a value from 0 to 4
+int value2 = map(valueldr,0,1023,1,5);  //we assign a v
 
 
- if(valueldr++){
-  for(int i=0; i<value1; i++){
+ if(valueldr++){             //if the value of the potentiometer increases 
+  for(int i=0; i<value1; i++){  //switch on the led depending on the potentiometer value
     digitalWrite(9+i,HIGH);
   }
   
  }
  
- if(valueldr--){
-  for(int j=0; j<value2; j++){
+ if(valueldr--){                //if the value of the potentiometer increases
+  for(int j=0; j<value2; j++){    //switch off the led depending on the potentiometer value
    digitalWrite(12-j,LOW); 
   }
 
